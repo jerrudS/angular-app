b340782fd5021e83d722a58d6cdf141b7c3bddbf
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-09-18 21:26:28 -0700

    Added form validation.

--- app/schools/form.component.js
+++ app/schools/form.component.js
@@ -3,10 +3,20 @@ angular.
   component('formElement', {
     template:
       '<div class="jumbotron welcome">' +
-        '<form novalidate class="form-group">' +
-          '<label>Name: <input class="form-control" type="text" ng-model="user.name" /></label><br />' +
-          '<label>E-mail: <input class="form-control" type="email" ng-model="user.email" /></label><br />' +
-          '<label>Favorite Big Ten School:<input class="form-control" type="text" ng-model="user.preference" /></label><br />' +
+        '<form name="form" class="form-group" novalidate>' +
+          '<label>Name: <input class="form-control" type="text" name="uName" ng-model="user.name" required="" /></label><br />' +
+          '<div ng-show="form.$submitted || form.uName.$touched">' +
+            '<div ng-show="form.uName.$error.required">What\'s your name?</div>' +
+          '</div>' +
+          '<label>E-mail: <input class="form-control" type="email" name="uEmail" ng-model="user.email" required="" /></label><br />' +
+          '<div ng-show="form.$submitted || form.uEmail.$touched">' +
+            '<span ng-show="form.uEmail.$error.required">What\'s your email.</span>' +
+            '<span ng-show="form.uEmail.$error.email">This is not a valid email.</span>' +
+          '</div>' +
+          '<label>Favorite Big Ten School:<input class="form-control" type="text" name="uPref" ng-model="user.preference" required="" /></label><br />' +
+          '<div ng-show="form.$submitted || form.uPref.$touched">' +
+            '<div ng-show="form.uPref.$error.required">What\'s your favorite school?</div>' +
+          '</div>' +
           '<button id="save-button" class="btn btn-primary" type="submit" ng-click="$ctrl.update(user)">Save</button>' +
         '</form>' +
       '</div>',
@@ -17,5 +27,14 @@ angular.
         this.master = angular.copy(user);
         console.log(this.master)
       };
+      this.reset = function(form) {
+        if (form) {
+          form.$setPristine();
+          form.$setUntouched();
+        }
+        this.user = angular.copy(this.master);
+      };
+
+      this.reset();
     }
   });
